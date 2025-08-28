3afd602518edf774c0ccd619ec1ec0fea573911d
MikeTysonWillKnockUOut <linhtran.31241024922@st.ueh.edu.vn>
2025-08-28 10:16:29 +0700

    bt4 - Guessing number

--- bai tap 4 - Guessing number.py
+++ bai tap 4 - Guessing number.py
@@ -4,17 +4,24 @@ num_lose = 0
 money = 100
 while True:
     print('Computer thinks a number from 1 to 100')
+    print('Each game cost five bucks')
     comp_number = random.randint(1,100)
-
+    money -= 5
     level = int(input('choose level [1,2,3]?'))
-    #1: easy, 2: medium , 3: harđ
+    #1: easy, 2: medium , 3: hard
     times = 10 if level == 1 else 5 if level == 2 else 3
 
     for time in range(times):
         your_num = int(input("Enter your guessing number #" + str(time + 1) + ": "))
         if your_num == comp_number:
             num_win += 1
-            money += 5
+            #your prize if you win
+            if level == 1:
+                 money += 5
+            elif level == 2:
+                 money +=10
+            else:
+                money += 15
             print('You are Genius!!!')
             break
         else:
@@ -26,11 +33,11 @@ while True:
             num_lose += 1
             money -= 5
             print('------------------')
-            print('Game over!')
+            print(f'Game over!, the number was: {comp_number}')
             break
     print('---------------------------------------------')
-    print(f'You won {num_win} out of {times} times')
-    print(f'You Lose {num_lose} out of {times} times')
+    print(f'You won {num_win} times')
+    print(f'You Lose {num_lose} times')
     print(f'Your current money is {money}')
     if money == 0:
       print("Runs out of money")
